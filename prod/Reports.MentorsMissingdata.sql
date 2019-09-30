55fa372710ce3b75beddd9d93af1cf2da13236de
Luis Rios <15820888+lriosiii@users.noreply.github.com>
2019-09-30 12:16:29 -0400

    correct where clause returning too many records

--- prod/Reports.MentorsMissingdata.sql
+++ prod/Reports.MentorsMissingdata.sql
@@ -13,7 +13,7 @@ WHERE     ((a.Address1 IS NULL AND m.MentorStatusID in (1)) OR
                       (m.MentorStatusID in (1) AND a.City IS NULL) OR
                       (m.MentorStatusID in (1) AND a.StateID IS NULL) OR
                       (m.MentorStatusID in (1) AND a.ZipCode IS NULL) OR
-                      (m.MentorStatusID in (1) AND  DecryptByKey(M.EncryptedBirthDate) IS NULL) OR
+                      (m.MentorStatusID in (1) AND  CONVERT(varchar, DecryptByKeyAutoCert(cert_ID('Certificate1'), NULL, EncryptedBirthDate)) IS NULL) OR
                       (m.MentorStatusID in (1) AND m.HomePhoneNumber IS NULL AND m.MobilePhoneNumber IS NULL AND m.WorkPhoneNumber IS NULL) OR
                       (m.MentorStatusID in (1) AND m.EmailAddress IS NULL) OR
 					  (m.MentorStatusID in (1) AND m.firstname IS NULL) OR
