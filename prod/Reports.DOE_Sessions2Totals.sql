efccf30b30790bb5975024ea3bca1d9c01146de8
lriosiii <15820888+lriosiii@users.noreply.github.com>
2019-12-20 17:13:47 -0500

    more readable

--- prod/Reports.DOE_Sessions2Totals.sql
+++ prod/Reports.DOE_Sessions2Totals.sql
@@ -1,22 +1,31 @@
---Use o.OfficeName, Count(distinct sms.StudentID) as #OfStudentsWithSessions to get the number of student with a least one session for report.
-
-
-
-SELECT  DISTINCT	o.OfficeName, Count(distinct sms.StudentID) as #OfStudentsWithSessions , s.OfficeID
---sms.SessionDate,  sms.SessionDuration, s.StudentID, m.MentorID, s.LastName + ', ' + s.FirstName AS StudentName, s.MiddleName, m.LastName + ', ' + m.FirstName As MentorName,
---				  s.StudentStatusID, s.OfficeID, s.CurrentGradeLevelID, s.ContractSignedDate, ss.StudentStatusName,
---                     sch.SchoolName, c.CountyName, o.OfficeName, --LEFT(sms.SessionNote, 1) As Note,
---				  s.i3ControlGroup, i3StudyGroupMember, MentorStatusName
+SELECT  DISTINCT	
+	o.OfficeName, 
+	Count(distinct sms.StudentID) as #OfStudentsWithSessions, 
+	s.OfficeID
+    	--sms.SessionDate,  
+	--sms.SessionDuration, 
+	--s.StudentID, 
+	--m.MentorID, 
+	--s.LastName + ', ' + s.FirstName AS StudentName, 
+	--s.MiddleName, 
+	--m.LastName + ', ' + m.FirstName As MentorName,  
+	--s.StudentStatusID, 
+	--s.OfficeID, 
+	--s.CurrentGradeLevelID, 
+	--s.ContractSignedDate,
+	--ss.StudentStatusName, 
+	--sch.SchoolName, 
+	--c.CountyName, 
+	--o.OfficeName, 
+	--LEFT(sms.SessionNote, 1) As Note, s.i3ControlGroup, i3StudyGroupMember, MentorStatusName 
 FROM  Students.Students AS s
-INNER JOIN Lookups.StudentStatuses AS ss ON s.StudentStatusID = ss.StudentStatusID
-INNER JOIN Offices.Offices o ON s.OfficeID = o.OfficeID
-Inner JOIN Schools.Schools AS sch ON s.SchoolID = sch.SchoolID
-INNER JOIN Lookups.Counties AS c ON s.CountyID = c.CountyID
-LEFT OUTER JOIN Students.StudentMentors AS sm ON s.StudentID = sm.StudentID
---AND (sm.UnassignedDate < '2013-08-01' OR sm.UnassignedDate IS NULL)
---AND sm.MentorAssignmentTypeID = 1
-INNER JOIN Mentors.Mentors AS m ON sm.MentorID = m.MentorID
-LEFT OUTER JOIN Students.MentoringSessions AS sms ON s.StudentID = sms.StudentID And sms.SessionDuration > 0 And m.MentorID = sms.MentorID
+INNER JOIN Lookups.StudentStatuses AS ss ON s.StudentStatusID = ss.StudentStatusID 
+INNER JOIN Offices.Offices o ON s.OfficeID = o.OfficeID 
+INNER JOIN Schools.Schools AS sch ON s.SchoolID = sch.SchoolID 
+INNER JOIN Lookups.Counties AS c ON s.CountyID = c.CountyID 
+LEFT JOIN Students.StudentMentors AS sm ON s.StudentID = sm.StudentID --AND (sm.UnassignedDate < '2013-08-01' OR sm.UnassignedDate IS NULL) --AND sm.MentorAssignmentTypeID = 1 
+INNER JOIN Mentors.Mentors AS m ON sm.MentorID = m.MentorID 
+LEFT OUTER JOIN Students.MentoringSessions AS sms ON s.StudentID = sms.StudentID And sms.SessionDuration > 0 And m.MentorID = sms.MentorID 
 INNER JOIN Lookups.MentorStatuses ms ON m.MentorStatusID = ms.MentorStatusID
 WHERE s.StudentStatusID IN (1,3,4,5)
 	And s.IsDeleted = 0
