63d0f1f854af32f52549ecfab104f2e44b036cdb
lriosiii <15820888+lriosiii@users.noreply.github.com>
2020-01-21 10:53:19 -0500

    add dynamic reporting dates

--- prod/Reports.BSC__nMentorSessionAverages2.sql
+++ prod/Reports.BSC__nMentorSessionAverages2.sql
@@ -37,19 +37,19 @@ WITH -- Common CTE for total active students for the time period (for mentor mat
 
 		Select case when d.IsTransfer = 1 and d.Firstsession is null and d.TstartDates > d.EndDate then Null
 					when d.IsTransfer = 1 and d.Firstsession is null and d.TstartDates < d.EndDate then d.TstartDates
-					WHEN d.istransfer = 1 and d.tdate < '2019-09-16' THEN '2019-10-15'
-					WHEN d.istransfer = 1 and d.tdate > '2019-09-15' AND Convert(date,DATEADD(Day,30,d.tdate)) <= d.firstsession THEN Convert(date,DATEADD(Day,30,d.tdate))
-					WHEN d.istransfer = 1 and d.tdate > '2019-09-15' AND Convert(date,DATEADD(Day,30,d.tdate)) > d.firstsession AND d.firstsession <= '2019-10-15' THEN '2019-10-15'
-					WHEN d.istransfer = 1 and d.tdate > '2019-09-15' AND Convert(date,DATEADD(Day,30,d.tdate)) > d.firstsession THEN d.firstsession
+					WHEN d.istransfer = 1 and d.tdate < dbo.Sep16() THEN dbo.Oct15()
+					WHEN d.istransfer = 1 and d.tdate > dbo.Oct15() AND Convert(date,DATEADD(Day,30,d.tdate)) <= d.firstsession THEN Convert(date,DATEADD(Day,30,d.tdate))
+					WHEN d.istransfer = 1 and d.tdate > dbo.Oct15() AND Convert(date,DATEADD(Day,30,d.tdate)) > d.firstsession AND d.firstsession <= dbo.Oct15() THEN dbo.Oct15()
+					WHEN d.istransfer = 1 and d.tdate > dbo.Oct15() AND Convert(date,DATEADD(Day,30,d.tdate)) > d.firstsession THEN d.firstsession
 
 				    --when ss.ContractSignedDate < '2017-09-16' then Convert (date, '2017-10-15')
-				    when d.IsTransfer = 0 and d.ContractSignedDate < '2019-09-16' then d.TstartDates
-					when d.IsTransfer = 0 and d.ContractSignedDate >= '2019-09-16' and d.FirstSession is null and Convert (date, DATEADD(Day,30,d.ContractSignedDate)) >= d.EndDate then null
-					when d.IsTransfer = 0 and d.ContractSignedDate >= '2019-09-16' and d.FirstSession is null and Convert (date, DATEADD(Day,30,d.ContractSignedDate)) < d.EndDate then Convert (date, DATEADD(Day,30,d.ContractSignedDate))
-					when d.IsTransfer = 0 and d.ContractSignedDate >= '2019-09-16' and d.FirstSession < '2019-10-15' then Convert (date,'2019-10-15')
+				    when d.IsTransfer = 0 and d.ContractSignedDate < dbo.Sep16() then d.TstartDates
+					when d.IsTransfer = 0 and d.ContractSignedDate >= dbo.Sep16() and d.FirstSession is null and Convert (date, DATEADD(Day,30,d.ContractSignedDate)) >= d.EndDate then null
+					when d.IsTransfer = 0 and d.ContractSignedDate >= dbo.Sep16() and d.FirstSession is null and Convert (date, DATEADD(Day,30,d.ContractSignedDate)) < d.EndDate then Convert (date, DATEADD(Day,30,d.ContractSignedDate))
+					when d.IsTransfer = 0 and d.ContractSignedDate >= dbo.Sep16() and d.FirstSession < '2019-10-15' then Convert (date,dbo.Oct15())
 					--when ss.ContractSignedDate >= '2017-09-16' and fmsd.FirstSessionDate > '2017-10-15' then Convert (date,fmsd.FirstSessionDate) took out and split into 2 categories below -DR
-					when d.IsTransfer = 0 and d.ContractSignedDate >= '2019-09-16' and d.FirstSession > Convert (date, DATEADD(Day,30,d.ContractSignedDate)) then Convert (date, DATEADD(Day,30,d.ContractSignedDate))
-					when d.IsTransfer = 0 and d.ContractSignedDate >= '2019-09-16' and d.FirstSession <= Convert (date, DATEADD(Day,30,d.ContractSignedDate)) then Convert (date,d.FirstSession)
+					when d.IsTransfer = 0 and d.ContractSignedDate >= dbo.Sep16() and d.FirstSession > Convert (date, DATEADD(Day,30,d.ContractSignedDate)) then Convert (date, DATEADD(Day,30,d.ContractSignedDate))
+					when d.IsTransfer = 0 and d.ContractSignedDate >= dbo.Sep16() and d.FirstSession <= Convert (date, DATEADD(Day,30,d.ContractSignedDate)) then Convert (date,d.FirstSession)
 
 
 					else ''
