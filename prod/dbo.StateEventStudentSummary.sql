c24f7080fbf8ff8567b00517bc58b02700dbee6c
lriosiii <15820888+lriosiii@users.noreply.github.com>
2020-02-13 11:46:51 -0500

    Separate from STAR view dependencies

--- prod/dbo.StateEventStudentSummary.sql
+++ prod/dbo.StateEventStudentSummary.sql
@@ -1,10 +1,10 @@
 SELECT
 	officename,
 	eventname,
-	SUM(CASE WHEN attended = 'Y' THEN 1 ELSE 0 END) AS attendees,
-	SUM(CASE WHEN invited = 'Y' THEN 1 ELSE 0 END) AS invitees,
+	SUM(CASE WHEN attended = 1 THEN 1 ELSE 0 END) AS attendees,
+	SUM(CASE WHEN invited = 1 THEN 1 ELSE 0 END) AS invitees,
 	(SELECT COUNT(*) FROM students.students s
 		INNER JOIN Lookups.StudentStatuses SS ON S.StudentStatusID = SS.StudentStatusID
 		WHERE s.IsDeleted = 0 and s.officeid=seslv.officeid and s.currentgradelevelid = 12 and ss.StatusGroupID = 1 ) AS activeseniorcount
-FROM dbo.StateEventStudentListView seslv
+FROM Reports.StateEventStudentListView seslv
 GROUP BY officeid, officename, eventname
