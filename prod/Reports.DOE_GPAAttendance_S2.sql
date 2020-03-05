9b29c9a194a7261c5886de116f1ee66490b7030e
lriosiii <15820888+lriosiii@users.noreply.github.com>
2020-03-05 16:03:58 -0500

    cleanup

--- prod/Reports.DOE_GPAAttendance_S2.sql
+++ prod/Reports.DOE_GPAAttendance_S2.sql
@@ -1,15 +1,3 @@
-/*
-TIME OF YEAR UPDATES - Made Summer 2019
-BEFORE:And sg.SemesterEndDate Between '2018-11-01' And '2019-03-31'
-AFTER: And sg.SemesterEndDate Between '2019-05-01' And '2019-07-31'
-BEFORE: And (sa.SemesterEndDate Between '2018-11-01' And '2019-03-31') 
-AFTER: And (sa.SemesterEndDate Between '2019-05-01' And '2019-07-31') 
-BEFORE: ContractSignedDate < '2018-11-01'
-AFTER: ContractSignedDate < '2019-03-31'
-BEFORE: And sg.SchoolTermTypeID in (17, 30, 0)
-AFTER: And sg.SchoolTermTypeID in (18, 30, 0)
-*/
-
 With totalStudentsWithGPACte (TotalStudentsWithGPA, StudentID, OfficeID) As 
 	(
 		Select Count(sg.StudentID) As TotalStudentsWithGPA,
@@ -28,7 +16,6 @@ With totalStudentsWithGPACte (TotalStudentsWithGPA, StudentID, OfficeID) As
 			And ss.ContractSignedDate  < dbo.Mar31()
 			
 		Group By sg.StudentID, ss.OfficeID
-		--Order By sg.StudentID --for testing
 	)
 
 	,totalStudentsWithAttendanceCte (TotalStudentsWithAttendance, StudentID, OfficeID) As 
@@ -37,10 +24,8 @@ With totalStudentsWithGPACte (TotalStudentsWithGPA, StudentID, OfficeID) As
 			sa.studentID,
 			ss.OfficeID
 		From Students.Attendance sa 
-			Join Students.Students ss
-				On ss.StudentID = sa.StudentID
-		Where ss.StudentStatusID 
-				In (1, 3, 4, 5) -- All active except "On Hold" 
+		Join Students.Students ss On ss.StudentID = sa.StudentID
+		Where ss.StudentStatusID In (1, 3, 4, 5) -- All active except "On Hold" 
 			And ss.CurrentGradeLevelID  IS NOT NULL
 			And (sa.SemesterEndDate Between dbo.May1() And (dbo.YearEndYYYYstring() + '-07-31'))
 			And sa.SchoolTermTypeID in (18, 30, 0) 
@@ -49,23 +34,18 @@ With totalStudentsWithGPACte (TotalStudentsWithGPA, StudentID, OfficeID) As
 			And ss.ContractSignedDate < dbo.Mar31()
 		Group By sa.StudentID, ss.OfficeID
 	)
-	
- 
+
 	 --CTE collects count of all students before a specific signing date
 	 ,totalStudentsCte (TotalStudents, OfficeID) As 
 	(
 		Select Count(ss.StudentID) As TotalStudents, 
 			ss.OfficeID
 		From Students.Students ss
-		Where ss.StudentStatusID 
-				In (1, 3, 4, 5) -- All active except "On Hold" 
-				And ss.IsDeleted = 0
-				And ContractSignedDate < dbo.Mar31()
-						
+		Where ss.StudentStatusID In (1, 3, 4, 5) -- All active except "On Hold" 
+			And ss.IsDeleted = 0
+			And ContractSignedDate < dbo.Mar31()					
 		Group By ss.OfficeID
-	)
-
-	
+	)	
 
 	--CTE to get total students with a GPA for dates and term types listed.
 	, totalStudentsWGPAByOffice (TotalWithGPA, OfficeID) AS
@@ -85,7 +65,6 @@ With totalStudentsWithGPACte (TotalStudentsWithGPA, StudentID, OfficeID) As
 		Group BY tcrcte.OfficeID
 	)
 	
-
 SELECT      oo.OfficeName, tcrStuCte.TotalStudents, tcrOffcte.TotalWithGPA,
 			Convert(Int, Ceiling((IsNull(tcrOffcte.TotalWithGPA, 0) / Cast(tcrStuCte.TotalStudents As Decimal)) * 100)) As PercentWithGPA,
 			tcrAttcte.TotalWithAttendance,
@@ -98,4 +77,3 @@ LEFT OUTER JOIN totalStudentsWAttendanceByOffice tcrAttcte ON oo.OfficeID = tcrA
 			
 WHERE		oo.IsDeleted = 0
 
---ORDER BY    OfficeName
