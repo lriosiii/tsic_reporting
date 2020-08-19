99a636feb8ac3702410f3db7803b8a9d154dd756
lriosiii <15820888+lriosiii@users.noreply.github.com>
2020-08-19 09:35:33 -0400

    Update Reports.MentorsMissingdata.sql

--- prod/Reports.MentorsMissingdata.sql
+++ prod/Reports.MentorsMissingdata.sql
@@ -9,25 +9,25 @@ FROM         Mentors.Mentors AS m LEFT OUTER JOIN
                       Common.Addresses AS a ON m.AddressID = a.AddressID INNER JOIN
                       Lookups.MentorStatuses AS ms ON m.MentorStatusID = ms.MentorStatusID INNER JOIN
 					  Offices.Offices o ON m.officeID = o.OfficeID
-WHERE     ((a.Address1 IS NULL AND m.MentorStatusID in (1)) OR
-                      (m.MentorStatusID in (1) AND a.City IS NULL) OR
-                      (m.MentorStatusID in (1) AND a.StateID IS NULL) OR
-                      (m.MentorStatusID in (1) AND a.ZipCode IS NULL) OR
-                      (m.MentorStatusID in (1) AND  CONVERT(varchar, DecryptByKeyAutoCert(cert_ID('Certificate1'), NULL, EncryptedBirthDate)) IS NULL) OR
-                      (m.MentorStatusID in (1) AND m.HomePhoneNumber IS NULL AND m.MobilePhoneNumber IS NULL AND m.WorkPhoneNumber IS NULL) OR
-                      (m.MentorStatusID in (1) AND m.EmailAddress IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.firstname IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.LastName IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.Gender IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.SalutationID IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.CountyID IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.RaceID IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.JoinReasonID IS NULL) OR
+WHERE     ((a.Address1 IS NULL AND m.MentorStatusID in (1,11)) OR
+                      (m.MentorStatusID in (1,11) AND a.City IS NULL) OR
+                      (m.MentorStatusID in (1,11) AND a.StateID IS NULL) OR
+                      (m.MentorStatusID in (1,11) AND a.ZipCode IS NULL) OR
+                      (m.MentorStatusID in (1,11) AND  CONVERT(varchar, DecryptByKeyAutoCert(cert_ID('Certificate1'), NULL, EncryptedBirthDate)) IS NULL) OR
+                      (m.MentorStatusID in (1,11) AND m.HomePhoneNumber IS NULL AND m.MobilePhoneNumber IS NULL AND m.WorkPhoneNumber IS NULL) OR
+                      (m.MentorStatusID in (1,11) AND m.EmailAddress IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.firstname IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.LastName IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.Gender IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.SalutationID IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.CountyID IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.RaceID IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.JoinReasonID IS NULL) OR
 					 -- (m.MentorStatusID in (1,6)) AND (mAPP.BackgroundCheckPassed IS NULL) OR
-					  (m.MentorStatusID in (1) AND mapp.BackgroundCheckReceivedDate IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND mapp.BackgroundCheckReceivedDate IS NULL) OR
 					  --(m.MentorStatusID in (1,6)) AND (mapp.TrainingCompleted IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.TrainingCompletedDate IS NULL) OR
-					  (m.MentorStatusID in (1) AND m.FingerPrintDate IS NULL))
+					  (m.MentorStatusID in (1,11) AND m.TrainingCompletedDate IS NULL) OR
+					  (m.MentorStatusID in (1,11) AND m.FingerPrintDate IS NULL))
 					  And m.IsDeleted = 0 and mapp.ApplicationStartDate > '2013-12-31'
 --AND m.OfficeID = 52
 
