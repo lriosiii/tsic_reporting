c35bbd82d446b26e9128c74ab9a0faead1151fd2
lriosiii <15820888+lriosiii@users.noreply.github.com>
2020-09-21 17:07:07 -0400

    fix: wasn't including unmatched mentors

--- prod/Reports.DOE_Sessions2TotalsByQuarter.sql
+++ prod/Reports.DOE_Sessions2TotalsByQuarter.sql
@@ -18,20 +18,23 @@ SELECT  DISTINCT
 	--c.CountyName, 
 	--o.OfficeName, 
 	--LEFT(sms.SessionNote, 1) As Note, s.i3ControlGroup, i3StudyGroupMember, MentorStatusName 
-FROM  Students.Students AS s
-INNER JOIN Lookups.StudentStatuses AS ss ON s.StudentStatusID = ss.StudentStatusID 
-INNER JOIN Offices.Offices o ON s.OfficeID = o.OfficeID 
-INNER JOIN Schools.Schools AS sch ON s.SchoolID = sch.SchoolID 
-INNER JOIN Lookups.Counties AS c ON s.CountyID = c.CountyID 
-LEFT JOIN Students.StudentMentors AS sm ON s.StudentID = sm.StudentID --AND (sm.UnassignedDate < '2013-08-01' OR sm.UnassignedDate IS NULL) --AND sm.MentorAssignmentTypeID = 1 
-INNER JOIN Mentors.Mentors AS m ON sm.MentorID = m.MentorID 
-LEFT OUTER JOIN Students.MentoringSessions AS sms ON s.StudentID = sms.StudentID And sms.SessionDuration > 0 And m.MentorID = sms.MentorID 
+FROM Students.Students AS s
+INNER JOIN Lookups.StudentStatuses AS ss ON s.StudentStatusID = ss.StudentStatusID
+INNER JOIN Offices.Offices o ON s.OfficeID = o.OfficeID
+Inner JOIN Schools.Schools AS sch ON s.SchoolID = sch.SchoolID
+INNER JOIN Lookups.Counties AS c ON s.CountyID = c.CountyID
+--LEFT OUTER JOIN Students.StudentMentors AS sm ON s.StudentID = sm.StudentID
+--AND (sm.UnassignedDate < '2013-08-01' OR sm.UnassignedDate IS NULL)
+--AND sm.MentorAssignmentTypeID = 1
+LEFT OUTER JOIN Students.MentoringSessions AS sms
+             ON s.StudentID = sms.StudentID And sms.SessionDuration > 0 --And m.MentorID = sms.MentorID
+INNER JOIN Mentors.Mentors AS m ON sms.MentorID = m.MentorID
 INNER JOIN Lookups.MentorStatuses ms ON m.MentorStatusID = ms.MentorStatusID
-WHERE s.StudentStatusID IN (1,3,4,5)
-	And s.IsDeleted = 0
-	And m.IsDeleted = 0
-	And sm.IsDeleted = 0
-	And sms.IsDeleted = 0
+WHERE s.StudentStatusID IN (1, 3, 4, 5)
+  And s.IsDeleted = 0
+  And m.IsDeleted = 0
+  --And sm.IsDeleted = 0
+  And sms.IsDeleted = 0
 	-- For Testing Building a way to get dups out.
 	And SessionDate Between dbo.CurrentQuarterStart() AND dbo.CurrentQuarterEnd()
 	And (sms.SessionNote IS NOT NULL OR sms.SessionNote <> '')
